8a6f5300ba36e9df6caf3d209136c1ea567ae8ad
Kareem <kareem.dana@gmail.com>
2013-06-05 19:58:48 -0500

    Moved MetaCriticScraper to its own class

--- metacritc.py
+++ metacritc.py
@@ -1,43 +1,20 @@
-from bs4 import BeautifulSoup
 import sys
-import urllib2
+from MetaCriticScraper import MetaCriticScraper
+import time
+# Do we want to handle movies, tv shows, etc...?
+# FIXME: Handle URL better
 
-url = urllib2.urlopen(sys.argv[1])
-html = url.read()
-
-soup = BeautifulSoup(html)
-
-product_title_div = soup.find("div", class_="product_title")
-title = product_title_div.a.text
-platform = product_title_div.span.a.text
-
-publisher = soup.find("li", class_="summary_detail publisher").a.text
-release_date = soup.find("li", class_="summary_detail release_data").find("span", class_="data").text
-
-critics = soup.find("div", class_="details main_details")
-critic_score = critics.find("span", class_="score_value").text
-critic_outof = critics.find("span", class_="score_total").span.text
-critic_count = critics.find("span", class_="count").a.span.text
-
-users = soup.find("div", class_="details side_details")
-users_score = users.find("span", class_="score_value").text
-raw_users_count = users.find("span", class_="count").a.text
-users_count = ""
-for c in raw_users_count:
-	if c.isdigit(): users_count += c
-
-product_info = soup.find("div", class_="section product_details").find("div", class_="details side_details")
-developer = product_info.find("li", class_="summary_detail developer").find("span", class_="data").text
-genre = product_info.find("li", class_="summary_detail product_genre").find("span", class_="data").text
-rating = product_info.find("li", class_="summary_detail product_rating").find("span", class_="data").text
-
-print "URL: " + url.geturl()
-print "Title: " + title.strip()
-print "Platform: " + platform.strip()
-print "Publisher: " + publisher.strip()
-print "Release Date: " + release_date.strip()
-print "Critic Score: " + critic_score.strip() + "/" + critic_outof + " (" + critic_count + " critics)"
-print "User Score: " + users_score.strip() + " (" + users_count + " users)"
-print "Developer: " + developer.strip()
-print "Genre: " + genre.strip()
-print "Rating: " + rating.strip()
+start_time = time.time()
+scraper = MetaCriticScraper(sys.argv[1])
+elapsed_time = time.time() - start_time
+print "URL: " + scraper.game['url']
+print "Title: " + scraper.game['title']
+print "Platform: " + scraper.game['platform']
+print "Publisher: " + scraper.game['publisher']
+print "Release Date: " + scraper.game['release_date']
+print "Critic Score: " + scraper.game['critic_score'] + "/" + scraper.game['critic_outof'] + " (" + scraper.game['critic_count'] + " critics)"
+print "User Score: " + scraper.game['user_score'] + " (" + scraper.game['user_count'] + " users)"
+print "Developer: " + scraper.game['developer']
+print "Genre: " + scraper.game['genre']
+print "Rating: " + scraper.game['rating']
+print "Time to scrape: ", round(elapsed_time, 2), "secs"
